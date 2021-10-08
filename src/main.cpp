27f140b7fd8d24fd66478235cb3ad57195bd6c13
jecndlria <jecndlria@gmail.com>
2021-10-08 11:41:27 -0700

    Fixed include messages to correctly search in the correct directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "c-echo.h"
-#include "c-count.h"
+#include "../header/c-echo.h"
+#include "../header/c-count.h"
 
 int main(int argv, char** argc) {
     std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
